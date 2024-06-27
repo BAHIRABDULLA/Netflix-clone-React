fe0cf51c8a75d1a68fa9cf5e0e5f24e6e1b800e2
Bahir Abdulla <bahirk880@gmail.com>
2024-06-28 01:45:15 +0530

    second commit

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,24 +1,50 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
+import axios from 'axios';
 import cards_data from '../../assets/cards/Cards_data'
-const TitleCards = ({title,category}) => {
+const TitleCards = ({ title, category }) => {
   const cardsRef = useRef()
-  const handleWheel = (event)=>{
+
+  const [apiData ,setApiData]= useState([])
+
+  const options = {
+    method: 'GET',
+    url: `https://api.themoviedb.org/3/movie/${category}?category:'now_playing`,
+    params: { language: 'en-US', page: '1' },
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MjY2MDg1NTI3YWUxYzZmMTI3YzQ0NTYxYzk2NjIxNSIsIm5iZiI6MTcxOTUxNDgyNC43MDAzNDksInN1YiI6IjY2N2RhZWMwYmZmNTMzYmEzMmQ1ZTFiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lHgfGsby8ay4_rcZa4doamxJcB5X_9CR4u4srwuCJuA'
+    }
+  };
+
+
+
+  const handleWheel = (event) => {
     event.preventDefault()
-    cardsRef.current.scrollLeft+= event.deltaY
+    cardsRef.current.scrollLeft += event.deltaY
   }
-  useEffect(()=>{
-    cardsRef.current.addEventListener('wheel',handleWheel)
-  })
+  useEffect(() => {
+    axios
+      .request(options)
+      .then(function (response) {
+        // console.log(response.data);
+        setApiData(response.data.results)
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+
+    cardsRef.current.addEventListener('wheel', handleWheel)
+  },[])
   return (
     <div className='title-cards'>
-      <h2>{title?title:'Popular on Netflix'}</h2>
+      <h2>{title ? title : 'Popular on Netflix'}</h2>
       <div className="card-list" ref={cardsRef}>
         {
-          cards_data.map((card,index)=>{
+          apiData.map((card, index) => {
             return <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img src={`https://image.tmdb.org/t/p/original`+card.backdrop_path} alt="" />
+              <p>{card.original_title}</p>
             </div>
           })
         }
